d7a90dd7e9b66ca33538328afcb80770f88ba777
Mohd Zaid Khan <mdzaidkhan628@gmail.com>
2022-01-01 13:49:31 +0530

    Setup postgres configurations in resumeMaker/settings.py

--- ResumeMaker/settings.py
+++ ResumeMaker/settings.py
@@ -78,8 +78,17 @@ WSGI_APPLICATION = 'ResumeMaker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        
+        'NAME': 'resumemaker',
+
+        'USER': 'postgres',
+
+        'PASSWORD': '1234',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
     }
 }
 
