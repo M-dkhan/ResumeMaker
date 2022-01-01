ea41a756271d92527a81ba6c653b7cbcd1a62840
Mohd Zaid Khan <mdzaidkhan628@gmail.com>
2022-01-01 16:39:48 +0530

    Added UserAttachment.models to admin.py  panel

--- UserAttachment/admin.py
+++ UserAttachment/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import WorkExperince 
+from .models import WorkExperince,Education,language,skill,Intrest,Certification,Achivement,CustomSection
 
 # Register your models here.
 admin.site.register(WorkExperince)
+admin.site.register(Education)
+admin.site.register(language)
+admin.site.register(skill)
+admin.site.register(Intrest)
+admin.site.register(Certification)
+admin.site.register(Achivement)
+admin.site.register(CustomSection)
