24435ef9124dd87840fa3d435b1fd2d2b77b8802
Mohd Zaid Khan <mdzaidkhan628@gmail.com>
2022-01-01 15:59:55 +0530

    Added Education model in userattachments.models.py

--- UserAttachment/models.py
+++ UserAttachment/models.py
@@ -16,3 +16,18 @@ class WorkExperince(models.Model):
 
     def __str__(self):
         return self.id
+
+
+class Education(mdoels.Model):
+    custom_user = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
+    degree = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    institution_name = models.CharField(max_length=200)
+    start_date = models.DateField() 
+    end_date = models.DateField()
+    description = models.TextField()    
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.id
